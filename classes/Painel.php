3052a2ae3a0772f34ab00352b2106043287387bf
leonardoanthony.dev <leonardoanthony.dev@gmail.com>
2022-03-15 20:20:48 -0300

    Criação da classe que irá gerenciar o painel de controle

---
+++ classes/Painel.php
@@ -0,0 +1,17 @@
+<?php
+
+    class Painel {
+
+        //Define se o usuário está logado ou não
+        public static function logado(){
+            return isset($_SESSION['login']) ? true : false;
+        }
+
+        // Encerra a sessão e redireciona para a pagina principal
+        public static function loggout(){
+            session_destroy();
+            header('Location: '.INCLUDE_PATH_PAINEL);
+        }
+    }
+
+?>
