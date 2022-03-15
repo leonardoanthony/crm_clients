987a57674bf79094d411b7b74b7736699fcdbc85
leonardoanthony.dev <leonardoanthony.dev@gmail.com>
2022-03-15 20:15:41 -0300

    criação de arquivo de configuração

---
+++ control.php
@@ -0,0 +1,13 @@
+<?php 
+
+define('INCLUDE_PATH','http://localhost/crm_clients/'); 
+define('INCLUDE_PATH_PAINEL',INCLUDE_PATH.'painel/');
+define('BASE_DIR_PAINEL',__DIR__."/painel");
+
+define('HOST','localhost');
+define('DB','crm-clients');
+define('USER','root');
+define('PASS','');
+
+
+?>
