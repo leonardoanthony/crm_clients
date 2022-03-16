4dbdcc4a45b0c9810a840621c2e70fc96e61cf74
leonardoanthony.dev <leonardoanthony.dev@gmail.com>
2022-03-16 02:00:20 -0300

    Front-end lista de clientes

---
+++ pages/listar-clientes.php
@@ -0,0 +1,22 @@
+<div class="container">
+    <div class="box-init">
+        <h3>Clientes Cadastrados</h3>
+        <table class="table">
+            <thead>
+                <tr>
+                    <th scope="col">Nome</th>
+                    <th scope="col">Categoria</th>
+                    <th scope="col">Menu</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <th>Leonardo</th>
+                    <td>Grátis</td>
+                    <td>Menu</td>
+                </tr>
+
+            </tbody>
+        </table>
+    </div>
+</div>
