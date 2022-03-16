6b3e8a0d5a021e162098b54471e525a05205b025
leonardoanthony.dev <leonardoanthony.dev@gmail.com>
2022-03-15 22:25:53 -0300

    Método de carregamento de páginas dinâmico

--- classes/Painel.php
+++ classes/Painel.php
@@ -2,7 +2,7 @@
 
      class Painel {
 
-        //Define se o usuário está logado ou não
+        // Define se o usuário está logado ou não
         public static function logado(){
             return isset($_SESSION['login']) ? true : false;
         }
@@ -13,10 +13,24 @@
             header('Location: '.INCLUDE_PATH);
         }
 
+        // Redireciona para outra página
         public static function redirect($url){
             echo '<script>location.href="'.$url.'"</script>';
             die();
+        }
 
+        // Carrega outras páginas mantendo header/footer
+        public static function loadPage(){
+            if (isset($_GET['url'])) {
+                $url = explode('/', $_GET['url']);
+                if (file_exists('pages/' . $url[0] . '.php')) {
+                    include('pages/' . $url[0] . '.php');
+                } else {
+                    header('Location: ' . INCLUDE_PATH_PAINEL);
+                }
+            } else {
+                include('pages/home.php');
+            }
         }
     }
 
