b6a696c5766ebf5763efdeb95f83db9ace71835b
leonardoanthony.dev <leonardoanthony.dev@gmail.com>
2022-03-16 00:25:19 -0300

    Requisitar classe Usuário

--- control.php
+++ control.php
@@ -1,10 +1,12 @@
 <?php 
 
 session_start();
+
 require_once('./classes/Painel.php');
 require_once('./classes/MySql.php');
+require_once('./classes/Usuario.php');
+
 define('INCLUDE_PATH','http://localhost/crm_clients/'); 
-define('INCLUDE_PATH_PAINEL',INCLUDE_PATH.'painel/');
 
 define('HOST','localhost');
 define('DB','crm-clients');
