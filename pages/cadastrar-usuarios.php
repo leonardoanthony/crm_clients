f9282602e69c710c511faaf907279fd798f9d1e8
leonardoanthony.dev <leonardoanthony.dev@gmail.com>
2022-03-16 00:25:49 -0300

    Lógica de cadastro de usuários

--- pages/cadastrar-usuarios.php
+++ pages/cadastrar-usuarios.php
@@ -1,25 +1,49 @@
+
+
+
 <div class="container">
     <div class="box-init">
+    <?php
+
+        if (@isset($_POST['acao'])){
+            $nome = $_POST['nome'];
+            $login = $_POST['login'];
+            $senha = $_POST['senha'];
+            $perfil = $_POST['perfil'];
+            
+            if(!$nome){
+                Painel::alert('erro','Nome não pode ser vazio');
+            }else if(!$login){
+                Painel::alert('erro','Login não pode ser vazio');
+            }else if(!$senha){
+                Painel::alert('erro','Senha não pode ser vazio');
+            }else if(!$perfil){
+                Painel::alert('erro','Selecione uma opção de perfil válida');
+            }else{
+                Usuario::cadastrarUsuario($nome, $login, $senha, $perfil);
+            }
+        }
+    ?>
         <h3>Cadastrar Usuário</h3>
         <form method="post">
             <div class="form-floating mb-3">
-                <input type="text" class="form-control" id="nome_completo" name="nome_completo" placeholder="Nome Completo">
-                <label for="nome_completo">Nome Completo</label>
+                <input type="text" class="form-control" value="<?php echo @$_POST['nome'];?>" id="nome" name="nome" placeholder="Nome Completo">
+                <label for="nome">Nome Completo</label>
             </div>
             <div class="form-floating mb-3">
-                <input type="text" class="form-control" id="login" name="login"  placeholder="Login">
+                <input type="text" class="form-control" value="<?php echo @$_POST['login'];?>" id="login" name="login"  placeholder="Login">
                 <label for="login">Login</label>
             </div>
             <div class="form-floating mb-3">
-                <input type="password" class="form-control" id="senha" name="senha" placeholder="Senha">
+                <input type="password" class="form-control" value="<?php echo @$_POST['senha'];?>" id="senha" name="senha" placeholder="Senha">
                 <label for="senha">Senha</label>
             </div>
 
             <div class="form-floating mb-3">
                 <select class="form-select" id="perfil" name="perfil" aria-label="Floating label select example">
-                    <option selected disabled>Selecione</option>
-                    <option value="0">Visualizador</option>
-                    <option value="1">Operador</option>
+                    <option value="0" selected>Selecione</option>
+                    <option value="1">Visualizador</option>
+                    <option value="2">Operador</option>
                 </select>
                 <label for="perfil">Perfil</label>
             </div>
